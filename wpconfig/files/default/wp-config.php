8ecce3406962aa85a678c1a1b4e0c2e51d0f94aa
Jesther Bas <dev.jestherbas@hotmail.com>
2016-01-27 18:39:31 +0800

    Update wp-config.php

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -16,8 +16,8 @@
 
 define('WP_REDIS_HOST', 'viicarttest.33s682.0001.use1.cache.amazonaws.com');
  
-define('WP_HOME','http://viicarttest-22120261.us-east-1.elb.amazonaws.com');
-define('WP_SITEURL','http://viicarttest-22120261.us-east-1.elb.amazonaws.com');
+define('WP_HOME','http://d1xgfbgoulgmlg.cloudfront.net');
+define('WP_SITEURL','http://d1xgfbgoulgmlg.cloudfront.net');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
